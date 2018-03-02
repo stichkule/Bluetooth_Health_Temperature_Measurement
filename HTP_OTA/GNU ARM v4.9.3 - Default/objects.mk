fa7944870242ce59ac4f6928e3ee4e2e7a67bdb5
Shiril Tichkule <31632517+stichkule@users.noreply.github.com>
2018-03-02 16:03:17 -0700

    Add files via upload

---
+++ HTP_OTA/GNU ARM v4.9.3 - Default/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/Users/shiril/SimplicityStudio/v4_workspace/HTP_OTA/protocol/bluetooth_2.7/lib/EFR32BG1B/GCC/libmbedtls.a C:/Users/shiril/SimplicityStudio/v4_workspace/HTP_OTA/platform/radio/rail_lib/autogen/librail_release/librail_efr32xg1_gcc_release.a C:/Users/shiril/SimplicityStudio/v4_workspace/HTP_OTA/protocol/bluetooth_2.7/lib/EFR32BG1B/GCC/libpsstore.a C:/Users/shiril/SimplicityStudio/v4_workspace/HTP_OTA/protocol/bluetooth_2.7/lib/EFR32BG1B/GCC/libbluetooth.a C:/Users/shiril/SimplicityStudio/v4_workspace/HTP_OTA/protocol/bluetooth_2.7/lib/EFR32BG1B/GCC/binapploader.o
+
+LIBS := -lm
+
